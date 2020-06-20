8ffe969d5f27ff8d7e4fbe539db5f27c6f7bd5fc
anhquangdesu <anhquangdesu@gmail.com>
2020-06-20 18:07:33 +0700

    user-regiter

--- src/app/layouts/user-regiter/user-regiter.component.ts
+++ src/app/layouts/user-regiter/user-regiter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MustMatch } from 'app/shared/helper/must-match-validator';
+import { LoginServiceService } from 'app/shared/service/login-service.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-regiter',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-regiter.component.css']
 })
 export class UserRegiterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  userObj: {
+    name: '',
+    email: '',
+    password: ''
+  };
+  errorMessage: String;
+  constructor(
+    private formBuilder: FormBuilder,
+    private _loginService: LoginServiceService,
+    private _router: Router,
+  ) {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
   }
 
+  ngOnInit() {
+
+  }
+  registerUser() {
+    this.submitted = true;
+    this.userObj = this.registerForm.value;
+    if (this.registerForm.invalid || this.userObj.name.trim() == '') {
+      this.errorMessage = 'Bạn hãy kiểm tra lại thông tin!'
+      return;
+    }
+    this._loginService.registerUser(this.userObj).subscribe((data) => {
+      const result = data.body
+      if (result['status'] === 200) {
+        // this.message = result['message'];
+        // const radio: HTMLElement = document.getElementById('modal-button20');
+        // radio.click();
+        setTimeout(() => {
+          this._router.navigate(['/']);
+        }, 5000);
+      } else if (result['status'] !== 200) {
+        this.errorMessage = result['message'];
+      }
+    });
+  }
 }
