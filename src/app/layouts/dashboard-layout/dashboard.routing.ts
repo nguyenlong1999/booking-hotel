3de8ebb4ea3a02b12b83f24ba7584bee645f233a
Vu_T-Solution <leanhvu86@gmail.com>
2020-06-20 18:38:16 +0700

    update

--- src/app/layouts/dashboard-layout/dashboard.routing.ts
+++ src/app/layouts/dashboard-layout/dashboard.routing.ts
@@ -3,109 +3,110 @@ import {DashboardLayoutComponent} from './dashboard-layout.component';
 import {IndexLayoutComponent} from './index-layout/index-layout.component';
 import {HotelRegisterComponent} from '../hotel-register/hotel-register.component';
 import {UserRegiterComponent} from "../user-regiter/user-regiter.component";
+
 export const DashBoardRoutes: Routes = [
 
-  {
-    path: '',
-    component: DashboardLayoutComponent,
-    children: [
-      {
+    {
         path: '',
-        redirectTo: 'index',
-        pathMatch: 'full'
-      },
-      {
-        path: 'index',
-        component: IndexLayoutComponent
-      },
-        // <= thêm children vào cho trang người dùng
-     {
-        path: 'hotel-register',
-        component: HotelRegisterComponent
-      },
-        {
-            path: 'user-register',
-            component: UserRegiterComponent
-        },
-      /* {
-        path: 'about',
-        component: AboutComponent
-      },
-      {
-        path: 'routing',
-        component: RoutingComponent
-      },
-      {
-        path: 'loginMember',
-        component: LoginComponent
-      },
-      {
-        path: 'combobox',
-        component: ComboBoxComponent
-      },
-      {
-        path: 'detail/:id',
-        component: RecipeDetailComponent
-      },
-      {
-        path: 'recipe',
-        component: RecipeComponent
-      },
-      {
-        path: 'active/:id',
-        component: WelcomeComponent
-      },
-      {
+        component: DashboardLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'index',
+                pathMatch: 'full'
+            },
+            {
+                path: 'index',
+                component: IndexLayoutComponent
+            },
+            // <= thêm children vào cho trang người dùng
+            {
+                path: 'hotel-register',
+                component: HotelRegisterComponent
+            },
+            {
+                path: 'user-register',
+                component: UserRegiterComponent
+            },
+            /* {
+              path: 'about',
+              component: AboutComponent
+            },
+            {
+              path: 'routing',
+              component: RoutingComponent
+            },
+            {
+              path: 'loginMember',
+              component: LoginComponent
+            },
+            {
+              path: 'combobox',
+              component: ComboBoxComponent
+            },
+            {
+              path: 'detail/:id',
+              component: RecipeDetailComponent
+            },
+            {
+              path: 'recipe',
+              component: RecipeComponent
+            },
+            {
+              path: 'active/:id',
+              component: WelcomeComponent
+            },
+            {
 
-        path: 'gallery',
-        component: GalleryComponent
-      }, {
+              path: 'gallery',
+              component: GalleryComponent
+            }, {
 
-        path: 'user/info/:id',
-        component: UserinforComponent
-      },
-      {
-        path: 'member/info/:id',
-        component: MemberinforComponent
-      },
-      {
-        path: 'personal/recipe/:id',
-        component: MyrecipeComponent
-      },
-      {
-        path: 'galleryDetail/:id',
-        component: GalleryDetailComponent
-      },
-      {
-        path: 'quydinh',
-        component: QuyDinhComponent
-      }
-      ,
-      {
-        path: 'chinhsachbaomat',
-        component: ChinhSachBaoMatComponent
-      },
-      {
-        path: 'chinhsachdiem',
-        component: ChinhsachdiemComponent
-      },
-      {
-        path: 'gioithieu',
-        component: ServiceComponent
-      }
-      ,
-      {
-        path: 'resetPassword',
-        component: ForgetPasswordComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'myGallery',
-        component: MygalleryComponent
-      }*/
-    ]
-  }
+              path: 'user/info/:id',
+              component: UserinforComponent
+            },
+            {
+              path: 'member/info/:id',
+              component: MemberinforComponent
+            },
+            {
+              path: 'personal/recipe/:id',
+              component: MyrecipeComponent
+            },
+            {
+              path: 'galleryDetail/:id',
+              component: GalleryDetailComponent
+            },
+            {
+              path: 'quydinh',
+              component: QuyDinhComponent
+            }
+            ,
+            {
+              path: 'chinhsachbaomat',
+              component: ChinhSachBaoMatComponent
+            },
+            {
+              path: 'chinhsachdiem',
+              component: ChinhsachdiemComponent
+            },
+            {
+              path: 'gioithieu',
+              component: ServiceComponent
+            }
+            ,
+            {
+              path: 'resetPassword',
+              component: ForgetPasswordComponent
+            },
+            {
+              path: 'register',
+              component: RegisterComponent
+            },
+            {
+              path: 'myGallery',
+              component: MygalleryComponent
+            }*/
+        ]
+    }
 ];
