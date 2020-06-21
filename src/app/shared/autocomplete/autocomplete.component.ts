ecf7d219db2cc0eb927f1298bff2bbe03e7fba1b
Vu_T-Solution <leanhvu86@gmail.com>
2020-06-22 00:39:47 +0700

    update chat box

--- src/app/shared/autocomplete/autocomplete.component.ts
+++ src/app/shared/autocomplete/autocomplete.component.ts
@@ -1,71 +1,82 @@
-import { Component, ViewChild, EventEmitter, Output, OnInit, AfterViewInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { } from 'googlemaps';
+import {Component, ViewChild, EventEmitter, Output, OnInit, AfterViewInit, Input} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {} from 'googlemaps';
 
 @Component({
-  selector: 'AutocompleteComponent',
-  template: `
-      <input class="input"
-        type="text"
-        [(ngModel)]="autocompleteInput"
-        #addresstext style="padding: 12px 20px; border: 1px solid #ccc; width: 400px"
+    selector: 'AutocompleteComponent',
+    template: `
+        <input class="input"
+               type="text"
+               [(ngModel)]="autocompleteInput"
+               #addresstext style="padding: 12px 20px; border: 1px solid #ccc; width: 400px"
         >
-      <div id="map" style="display: none;"></div>
-  `,
+        <div id="map" style="display: none;"></div>
+        <div id="info-content" style="display: none;"></div>
+    `,
 })
 export class AutocompleteComponent implements OnInit, AfterViewInit {
-  @Input() adressType: string;
-  @Output() setAddress: EventEmitter<any> = new EventEmitter();
-  @ViewChild('addresstext') addresstext: any;
+    @Input() adressType: string;
+    @Output() setAddress: EventEmitter<any> = new EventEmitter();
+    @ViewChild('addresstext') addresstext: any;
 
-  autocompleteInput: string;
-  queryWait: boolean;
-  private map;
-  constructor() {
-  }
+    autocompleteInput: string;
+    queryWait: boolean;
+    private map;
+    private infoWindow;
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  ngAfterViewInit() {
-    this.getPlaceAutocomplete();
-  }
+    ngAfterViewInit() {
+        this.getPlaceAutocomplete();
+    }
 
-  private getPlaceAutocomplete() {
-    const autocomplete = new google.maps.places.Autocomplete(this.addresstext.nativeElement,
-        {
-          // componentRestrictions: { country: 'US' },
-          types: [this.adressType]  // 'establishment' / 'address' / 'geocode'
+    private getPlaceAutocomplete() {
+        const autocomplete = new google.maps.places.Autocomplete(this.addresstext.nativeElement,
+            {
+                // componentRestrictions: { country: 'US' },
+                types: [this.adressType]  // 'establishment' / 'address' / 'geocode'
+            });
+        google.maps.event.addListener(autocomplete, 'place_changed', () => {
+            const place = autocomplete.getPlace();
+            this.invokeEvent(place);
+            console.log(place)
         });
-    google.maps.event.addListener(autocomplete, 'place_changed', () => {
-      const place = autocomplete.getPlace();
-      this.invokeEvent(place);
-    });
-  }
+    }
 
-  invokeEvent(place: Object) {
-      this.map= new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center:{ lat: 54.8, lng: -4.6 },
-          mapTypeControl: false,
-          panControl: false,
-          zoomControl: false,
-          streetViewControl: false
-      });
-    let  places = new google.maps.places.PlacesService(this.map);
-    let search = {
-    types: ['tourist_attraction']
-    };
-    places.nearbySearch(search, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results);
-
-        } else {
-        console.log(results);
+    invokeEvent(place: Object) {
+        this.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: new google.maps.LatLng(-33.8665433, 151.1956316),
+            mapTypeControl: false,
+            panControl: false,
+            zoomControl: false,
+            streetViewControl: false
+        });
+        this.infoWindow = new google.maps.InfoWindow({
+            content: document.getElementById('info-content')
+        });
+        if (place['geometry']) {
+            this.map.panTo(place['geometry'].location);
+            this.map.setZoom(7);
         }
-        });
-    this.setAddress.emit(place);
-    console.log(place);
-  }
+        let places = new google.maps.places.PlacesService(this.map);
+        let search = {
+            bounds: this.map.getBounds(),
+            types: ['tourist_attraction']
+        };
+        // places.nearbySearch(search, function (results, status,pagination) {
+        //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+        //         console.log(results);
+        //
+        //     } else {
+        //         console.log(results);
+        //     }
+        // });
+        this.setAddress.emit(place);
+        console.log(place);
+    }
 
 }
