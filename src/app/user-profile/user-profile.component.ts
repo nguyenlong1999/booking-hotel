bd2af265b02950cc82ae7f4e7e8ab18d860a938d
anhquangdesu <anhquangdesu@gmail.com>
2020-06-21 07:57:51 +0700

    update user-profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'app/shared/service/user.service.';
 import { User } from 'app/shared/model/user';
 import { ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,23 +11,52 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  userObject: {
+    id: '',
+    email: '',
+    name: '',
+    birthday: '',
+    gender: '',
+    materialStatus: '',
+    signature: '',
+    introduction: ''
+  };
   genders = [
-    { value: '0', viewValue: 'Nam' },
-    { value: '1', viewValue: 'Nữ' },
-    { value: '2', viewValue: 'Không xác định' }
+    { value: 0, viewValue: 'Nam' },
+    { value: 1, viewValue: 'Nữ' },
+    { value: 2, viewValue: 'Không xác định' }
+  ];
+  materials = [
+    { value: 0, viewValue: 'Đã ly hôn' },
+    { value: 1, viewValue: 'Đã kết hôn' },
+    { value: 2, viewValue: 'Đang yêu' },
+    { value: 3, viewValue: 'Chưa có người yêu' }
   ];
   years: number[] = [];
-  genderValue: String;
+  id: any;
   url: any;
   user: User;
-  year: '1950';
+  yearr: 1950;
   // email: String;
   imageUrl: String = 'jbiajl3qqdzshdw0z749';
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute,
-    private cookies: CookieService
-  ) { }
+    private cookies: CookieService,
+    private formBuilder: FormBuilder
+  ) {
+
+    this.profileForm = this.formBuilder.group({
+      id: [''],
+      email: [''],
+      name: [''],
+      birthday: [''],
+      gender: [''],
+      materialStatus: [''],
+      signature: [''],
+      introduction: ['']
+    });
+  }
 
   ngOnInit() {
     this.getMemerInfo();
@@ -34,89 +64,42 @@ export class UserProfileComponent implements OnInit {
   }
   getMemerInfo() {
     let email = this.cookies.get('email');
-    console.log(email);
-    // this.id = this.route.snapshot.params.id;
     this.userService.getMemberInfo(email).subscribe(user => {
-      console.log(user)
+      this.user = user;
+      this.id = user._id;
+      console.log(this.id)
+      this.profileForm.controls['id'].patchValue(user._id);
+      this.profileForm.controls['email'].patchValue(user.email);
+      this.profileForm.controls['name'].patchValue(user.name);
+      this.profileForm.controls['birthday'].patchValue(user.birthday);
+      this.profileForm.controls['gender'].patchValue(user.gender);
+      this.profileForm.controls['materialStatus'].patchValue(user.materialStatus);
+      this.profileForm.controls['signature'].patchValue(user.signature);
+      this.profileForm.controls['introduction'].patchValue(user.introduction);
+      console.log(this.profileForm.value);
       if (user !== undefined) {
         this.user = user;
-        this.genderValue = user.gender;
         if (user.imageUrl !== '') {
           this.imageUrl = user.imageUrl
         }
-        // this.recipeService.getRecipes().subscribe(recipes => {
-        //   if (recipes !== undefined) {
-        //     this.userObject.email = this.cookie.get('email')
-        //     if (this.userObject.email !== undefined || this.userObject.email !== '') {
-        //       this.recipeService.findInterest(this.userObject).subscribe(data => {
-        //         let interests = data.body['interests']
-
-
-
-        //         for (let recipe of recipes) {
-        //           if (recipe.user._id === user._id) {
-        //             recipe.like = false
-        //             if (recipe.hardLevel !== undefined) {
-        //               if (recipe.hardLevel === "") {
-        //                 recipe.hardLevel = "Không xác định";
-        //               } else if (recipe.hardLevel === "1") {
-        //                 recipe.hardLevel = "Dễ";
-        //               } else if (recipe.hardLevel === "2") {
-        //                 recipe.hardLevel = "Trung bình";
-        //               } else if (recipe.hardLevel === "3") {
-        //                 recipe.hardLevel = "Khó";
-        //               } else if (recipe.hardLevel === "4") {
-        //                 recipe.hardLevel = "Rất khó";
-        //               }
-        //             }
-        //             console.log(recipe.user.name)
-        //             this.recipeCount++
-        //             this.doneCount = this.doneCount + recipe.doneCount
-        //             this.viewCount = this.viewCount + recipe.viewCount
-        //             recipe.like = false
-        //             this.memberRecipes.push(recipe)
-
-        //             if (interests !== undefined) {
-        //               for (let interest of interests) {
-        //                 if (interest.objectId._id === recipe._id && interest.objectType === '2') {
-        //                   recipe.like = true
-        //                 }
-        //               }
-        //             }
-        //           }
-        //         }
-
-        //         this.galleryService.getGalleryies().subscribe(gallerys => {
-        //           if (gallerys !== undefined) {
-        //             for (let gallery of gallerys) {
-        //               if (gallery.user._id === user._id) {
-        //                 if (gallery.recipe.length > 0) {
-        //                   gallery.image = gallery.recipe[0].imageUrl
-        //                 } else {
-        //                   gallery.image = 'fvt7rkr59r9d7wk8ndbd'
-        //                 }
-        //                 this.memberGallery.push(gallery)
-        //               }
-        //               if (interests !== undefined) {
-        //                 for (let interest of interests) {
-        //                   if (interest.objectId._id === gallery._id && interest.objectType === '1') {
-        //                     gallery.like = true
-        //                   }
-        //                 }
-        //               }
-        //             }
-        //             this.galleryCount = this.memberGallery.length
-
-        //           }
-        //         })
-        //         this.memberInfo = user
-        //         this.infoCheck = true;
-        //       })
-        //     }
-        //   }
-        // });
       }
     })
+  }
+
+  updateProfile() {
+    console.log('submit');
+    this.userObject = this.profileForm.value;
+    this.userService.updateUser(this.userObject).subscribe(user => {
+      const status = user.body['status']
+      console.log(status)
+      if (status === 200) {
+        console.log(user)
+        if (this.user.signature !== undefined) {
+          this.user.signature = atob(this.user.signature)
+        }
+        console.log(this.user.signature);
+      }
+    });
   }
 
   onSelectFile(event) {
