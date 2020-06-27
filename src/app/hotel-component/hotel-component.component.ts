fedc41bb2b3a5020556039b513d401afd04e4edd
nguyenlong1999 <45970234+nguyenlong1999@users.noreply.github.com>
2020-06-27 15:55:59 +0700

    mat table hotels

--- src/app/hotel-component/hotel-component.component.ts
+++ src/app/hotel-component/hotel-component.component.ts
@@ -4,6 +4,7 @@ import {Hotel} from '../shared/model/hotel';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-hotel-component',
@@ -12,34 +13,47 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class HotelComponentComponent implements OnInit, AfterViewInit {
     hotels: Hotel[] = []
-    displayedColumns = ['id', 'name', 'progress', 'color'];
+    displayedColumns = ['id', 'name', 'sqm', 'add'];
 
 
     dataSource: MatTableDataSource<Hotel>;
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
 
-    constructor(private  hotelService: HotelService) {
+    constructor(private  hotelService: HotelService, private route: Router) {
     }
 
-    ngOnInit(): void {
-        this.getHotel()
-        this.dataSource = new MatTableDataSource(this.hotels)
+    async ngOnInit() {
+        await this.getHotel();
+    }
 
+    applyFilter(filterValue: string) {
+        filterValue = filterValue.trim(); // Remove whitespace
+        filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+        this.dataSource.filter = filterValue;
     }
 
     ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
+
     }
 
-    getHotel() {
-        this.hotelService.getHotels().subscribe(hotels => {
-            if (hotels === undefined) {
-                return;
-            }
-            this.hotels = hotels
-        })
+    onClickEditRow(row_id) {
+        console.log(row_id)
     }
+
+    getHotel(): Promise<any> {
+        return this.hotelService.getHotels().toPromise().then(hotels => {
+                console.log(hotels)
+                if (hotels === undefined) {
+                    return;
+                }
+                this.hotels = hotels;
+                this.dataSource = new MatTableDataSource(this.hotels)
+                this.dataSource.paginator = this.paginator;
+                this.dataSource.sort = this.sort;
+            }
+        )
+    }
+
 
 }
