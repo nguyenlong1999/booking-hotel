fd568b87c855ac2b2a70b712e0f66e7610660351
nguyenlong1999 <45970234+nguyenlong1999@users.noreply.github.com>
2020-07-05 18:01:15 +0700

    send mai giữa 2 thèng ngáo

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,18 +1,19 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-import { Location, PopStateEvent } from '@angular/common';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {Location, PopStateEvent} from '@angular/common';
 import 'rxjs/add/operator/filter';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 import PerfectScrollbar from 'perfect-scrollbar';
+
 import * as $ from 'jquery';
-import { TranslateService } from '@ngx-translate/core';
-import { Title } from '@angular/platform-browser';
-import { ChatService } from '../../shared/service/chat.service';
-import { Message } from '../../shared/model/message';
-import { CookieService } from 'ngx-cookie-service';
-import { UserService } from '../../shared/service/user.service.';
-import { User } from '../../shared/model/user';
-import { ChatMessage } from '../../shared/model/chat-message';
+import {TranslateService} from '@ngx-translate/core';
+import {Title} from '@angular/platform-browser';
+import {ChatService} from '../../shared/service/chat.service';
+import {Message} from '../../shared/model/message';
+import {CookieService} from 'ngx-cookie-service';
+import {UserService} from '../../shared/service/user.service.';
+import {User} from '../../shared/model/user';
+import {ChatMessage} from '../../shared/model/chat-message';
 
 @Component({
     selector: 'app-admin-layout',
@@ -24,33 +25,29 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     lastPoppedUrl: string;
     yScrollStack: number[] = [];
     newMessage = false;
-    messageEmpty = false;
     userMessages: ChatMessage[] = [];
-    userChatList: User[] = [];
+    userChatList: User [] = [];
     guessMessages: ChatMessage[] = [];
     @Input('ngModel') message;
     userOnline: String[] = [];
-    userObject = {
-        email: ''
-    }
     toUser = '';
 
     constructor(
         public location: Location, private router: Router,
         private translate: TranslateService,
         private title: Title,
-        private chatService: ChatService,
+        private  chatService: ChatService,
         private cookieService: CookieService,
         private userService: UserService
     ) {
         translate.setDefaultLang('vi');
         sessionStorage.setItem('currentLang', 'vi');
-        // this.mailBox();
+        this.mailBox();
         this.getListOnline();
         this.userService.getActiveUsers().subscribe(data => {
             console.log(data)
             this.userChatList = data;
-            const id = this.cookieService.get('ObjectId');
+            let id = this.cookieService.get('ObjectId');
             this.userChatList = this.userChatList.filter(user => user._id !== id);
             this.userChatList.forEach(user => {
                 user.online = false;
@@ -111,9 +108,10 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         }
 
         const window_width = $(window).width();
-        const $sidebar = $('.sidebar');
-        const $sidebar_responsive = $('body > .navbar-collapse');
-        const $sidebar_img_container = $sidebar.find('.sidebar-background');
+        let $sidebar = $('.sidebar');
+        let $sidebar_responsive = $('body > .navbar-collapse');
+        let $sidebar_img_container = $sidebar.find('.sidebar-background');
+
 
         if (window_width > 767) {
             if ($('.fixed-plugin .dropdown').hasClass('show-dropdown')) {
@@ -134,13 +132,13 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         });
 
         $('.fixed-plugin .badge').click(function () {
-            const $full_page_background = $('.full-page-background');
+            let $full_page_background = $('.full-page-background');
 
 
             $(this).siblings().removeClass('active');
             $(this).addClass('active');
 
-            const new_color = $(this).data('color');
+            var new_color = $(this).data('color');
 
             if ($sidebar.length !== 0) {
                 $sidebar.attr('data-color', new_color);
@@ -152,13 +150,13 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         });
 
         $('.fixed-plugin .img-holder').click(function () {
-            const $full_page_background = $('.full-page-background');
+            let $full_page_background = $('.full-page-background');
 
             $(this).parent('li').siblings().removeClass('active');
             $(this).parent('li').addClass('active');
 
 
-            const new_image = $(this).find('img').attr('src');
+            var new_image = $(this).find('img').attr('src');
 
             if ($sidebar_img_container.length != 0) {
                 $sidebar_img_container.fadeOut('fast', function () {
@@ -195,58 +193,6 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         message.message = 'get list user';
         this.chatService.getListMember(message);
     }
-
-    // mailBox() {
-    //     this.chatService.getMessages().subscribe(mail => {
-    //         // console.log('mail:', mail);
-    //         if (mail !== undefined) {
-    //             // this.newMessage = true;
-    //             const mess = new Message;
-    //             // mess.content = mail;
-    //             // mess.news = true;
-    //             console.log(mess);
-    //             if (mess['content']['get-list-online'] !== undefined) {
-    //                 console.log(mess['content']['get-list-online']);
-    //                 let userOnline = JSON.stringify(mess['content']['get-list-online']);
-    //                 userOnline = userOnline.substring(1);
-    //                 userOnline = userOnline.substring(0, userOnline.length - 1);
-    //                 const userOnlineArray = userOnline.split(',');
-    //                 userOnlineArray.forEach(user => {
-    //                     const tempArr = user.split(':');
-    //                     if (tempArr.length > 0) {
-    //                         let mystring = tempArr[0];
-    //                         mystring = mystring.substring(1);
-    //                         mystring = mystring.substring(0, mystring.length - 1);
-    //                         if (!this.userOnline.includes(mystring)) {
-    //                             this.userOnline.push(mystring);
-    //                         }
-    //                     }
-    //                 });
-    //                 this.userService.getActiveUsers().subscribe(data => {
-    //                     console.log(data)
-    //                     this.userChatList = data;
-    //                     const id = this.cookieService.get('ObjectId');
-    //                     this.userChatList = this.userChatList.filter(user => user._id !== id);
-    //                     this.userChatList.forEach(user => {
-    //                         this.userOnline.forEach(id => {
-    //                             if (id === user._id) {
-    //                                 user.online = true;
-    //                             }
-    //                         });
-    //                     });
-    //
-    //                     console.log(this.userOnline);
-    //                     console.log(this.userChatList);
-    //                 });
-    //             } else {
-    //                 this.userMessages.push(mess);
-    //                 // this.chatService.showNotification('success', mess.content);
-    //             }
-    //             console.log(this.userMessages);
-    //
-    //         }
-    //     })
-    // }
 
     mailBox() {
         this.chatService.getMessages().subscribe(mail => {
@@ -290,6 +236,9 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
                         console.log(this.userChatList);
                     });
                 } else {
+                    debugger;
+                    console.log(mess + 'longdz vãi loz')
+                    this.addChatBoxSendMessageV2(mail);
                     this.userMessages.push(mess);
                 }
                 console.log(this.userMessages);
@@ -316,7 +265,7 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         message.objectId = this.toUser;
         message.content = this.message;
         message.message = this.message;
-        const time = new Date();
+        let time = new Date();
         message.time = this.formatDate(time);
         this.userMessages.push(message);
         this.chatService.sendMessage(message).subscribe(data => {
@@ -329,6 +278,23 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
             this.message = '';
         });
         console.log(message)
+    }
+
+    addChatBoxSendMessageV2(message) {
+        const radio: HTMLElement = document.getElementById('msg_history');
+        radio.innerHTML += ' <div class="outgoing_msg" style="float: right;display:block;overflow:hidden;clear: both;">\n' +
+            '<div class="sent_msg" style="overflow:hidden; margin:6px 0 6px;">\n' +
+            '<p style="background: #05728f none repeat scroll 0 0;\n' +
+            '  border-radius: 3px;\n' +
+            '  font-size: 10px;\n' +
+            '  margin: 0; color:#fff;\n' +
+            '  padding: 5px 10px 5px 12px;\n' +
+            '  width:100%;\n' +
+            '  display: block;\n' +
+            '  text-align: left;">' + message + '</p>\n' +
+            '<span class="time_date" style="font-size: 8px;">' + 'không thích gửi time đấy' + '</span>' +
+            '</div>\n' +
+            '                                    </div>';
     }
 
     addChatBoxSendMessage(message) {
@@ -357,7 +323,7 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
         let message = new ChatMessage();
         message.objectId = this.cookieService.get('ObjectId');
         message.message = 'reponse';
-        const time = new Date();
+        let time = new Date();
         message.time = this.formatDate(time);
         this.guessMessages.push(message);
         this.chatService.sendMessage(message);
@@ -397,7 +363,7 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     }
 
     isMaps(path) {
-        let titlee = this.location.prepareExternalUrl(this.location.path());
+        var titlee = this.location.prepareExternalUrl(this.location.path());
         titlee = titlee.slice(1);
         if (path == titlee) {
             return false;
@@ -407,13 +373,13 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     }
 
     formatDate(dt) {
-        const normalizeHour = dt.getHours() >= 13 ? dt.getHours() - 12 : dt.getHours()
+        let normalizeHour = dt.getHours() >= 13 ? dt.getHours() - 12 : dt.getHours()
         return dt.getHours() >= 13 ? normalizeHour + ': ' + dt.getMinutes() + ' PM' : normalizeHour + ': ' + dt.getMinutes() + ' AM ' + dt.getDate() + '-' + dt.getMonth();
     }
 
     onActivate(event) {
-        const scrollToTop = window.setInterval(() => {
-            const pos = window.pageYOffset;
+        let scrollToTop = window.setInterval(() => {
+            let pos = window.pageYOffset;
             if (pos > 0) {
                 window.scrollTo(0, pos - 20); // how far to scroll on each step
             } else {
