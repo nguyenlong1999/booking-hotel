c84352b72584a0b8a164280e703784f740e7fd38
Hoàng Anh Quang <51349823+anhquangdesu@users.noreply.github.com>
2020-07-23 14:39:13 +0700

    Update booking.ts

--- src/app/shared/model/booking.ts
+++ src/app/shared/model/booking.ts
@@ -3,11 +3,13 @@ export class Booking {
     _id: Object;
     hotelNameSpace: string;
     name: string;
+    nameHotel: string;
     email: string;
     phone: string;
     roomDetailID: string;
     status: string;
     totalAmountRoom: number;
+    totalNight: number
     totalMoney: number;
     date: any;
     hotelObjId: string;
